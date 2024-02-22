63f7fa283dc9d014105dc33e833288c47c701acd
hosnaml <hosna@kth.se>
2024-02-22 12:03:40 +0100

    test function for parsing

--- W6/ranges.ex
+++ W6/ranges.ex
@@ -1,3 +1,22 @@
 def parser (descr) do
 
+  [seeds | maps] = String.split(descr, "\n\n");
+  [ _ | seeds] = String.split(seeds, " ");
+
+  seeds = Enum.map(seeds, fn(x) -> {nr, _} = Integer.parse(x); nr end)
+
+  maps = Enum.map(maps, fn(map) ->
+    [_ | transf] = String.split(map, "\n")
+    Enum.map(transf, fn(tr) ->
+      String.split(tr, " ")
+    maps
+    end)
+  {seeds, maps}
+  end)
+
+end
+
+def test() do
+  {:ok, content}  = File.read("input.txt")
+  parser(content);
 end
