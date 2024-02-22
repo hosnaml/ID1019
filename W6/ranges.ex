55a31b5fd35837fbe3b0dc395fc39087189d6ee1
hosnaml <hosna@kth.se>
2024-02-23 00:34:42 +0100

    parser function completely fixed

--- W6/ranges.ex
+++ W6/ranges.ex
@@ -3,41 +3,50 @@ defmodule Ranges do
   def parser (descr) do
 
     [seeds | maps] = String.split(descr, "\n\n")
-    seeds = String.trim(seeds, "seeds: ")
-    IO.puts(seeds)
     [ _ | seeds] = String.split(seeds, " ")
-    IO.puts(seeds)
-    seeds = Enum.map(seeds, fn(x) -> {nr, _} = Integer.parse(x); nr end)
 
+    seeds = Enum.map(seeds, fn(x) ->
+      {nr, _} = Integer.parse(x)
+      nr
+    end)
 
-    #maps = String.split(maps, "\n")
-    maps = Enum.drop(maps,1)
-    maps = Enum.map(maps, fn (line) ->
-      String.split(line, "\s")
-      Integer.parse(line)
-      |> then (fn [a, b, c, d] ->
-        [destination_range_start: a,
-             source_range_start: b,
-             range_length: c,
-             source_range: b..b+c-1,
-             distance: a-b]
-          end)
+    acc = []
 
+    maps = Enum.map(maps, fn(map) ->
+      [_ | transf] = String.split(map, "\n")
+
+      row = Enum.map(transf, fn r ->
+        r
+        |> String.split(" ")
+        |> Enum.map(fn s -> String.to_integer(s) end)
       end)
-    {seeds, maps}
+
+      acc = acc ++ [row]
+
+      #acc
+      #|> IO.inspect(charlists: :as_lists)
+
+    {seeds, acc}
+    end)
   end
 
     def test() do
-      {:ok, content}  = File.read("test.txt");
-      {seeds, maps} = parser(content)
-      #using comprehensions it loops over and for eahc seeds the function is called.
-      #for seeds <-
+      {:ok, content}  = File.read("test.txt")
+      [{seeds, maps} | tail] = parser(content)
+      #Enum.each(seeds, fn seed ->
+      #source_to_destination(maps, seed)
+        #Enum.min()
+      #end)
+
+    end
+
+    #if the map is empty is returns the seeds.
+    def source_to_destination([], seeds) do
+      seeds
     end
 
     def source_to_destination(maps, seeds) do
 
     end
-
-
 
 end
